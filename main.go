d2c6f127aedeec3a75b26d27432ccbd5c3dbed20
Kaycee Nuel <heykayceee@gmail.com>
2025-02-19 16:46:00 +0100

    added fetcher package and function for fetching weather data

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
-package main 
+package main
 
 import (
 	"fmt"
 	"os"
+
+	"output-and-error-handling/fetcher"
 )
 
 func main() {
-	if err := run(), err != nil {
-		fmt.FprintIn(os.stderr, err)
+	url := "http://localhost:8080" // server to run on this URL
+
+	weather, err := fetcher.FetchWeather(url)
+	if err != nil {
+		// Print error to stderr and exit withh nonzero codee
+		fmt.Fprintln(os.Stderr, "Error fetching weather:", err)
+		os.Exit(1)
 	}
-}
 
-
-func run() error {
-	retun nil
+	// print the weather data to stdout
+	fmt.Println(weather)
 }
